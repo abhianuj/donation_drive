5f3d8a2055c5311471239c7741a25ffc075303be
I524896 <abhijeet.kumar04@sap.com>
2021-10-15 17:52:51 +0530

    unit tests for fomatters done

--- ui/src/test/formatter.test.js
+++ ui/src/test/formatter.test.js
@@ -1,5 +1,21 @@
-const sum = require('./../utils/formatter');
+import { sum, returnFirstLetter, showIndianStandardCurrency, returnRemainingDaysLeft, donationPerecent } from './../utils/formatter';
+const formatter = require('./../utils/formatter');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+
+test('Should return the first letter of the word with capitalizaion', () => {
+  expect(returnFirstLetter("abhi")).toBe("A");
+});
+
+test('Should return the numbers in indian standard of displaying money with rupee symbol if the amount is 0', () => {
+  expect(showIndianStandardCurrency(0)).toBe("₹​" + 0);
+});
+test('Should return the numbers in indian standard of displaying money with rupee symbol if the amount is 10,00,000', () => {
+  expect(showIndianStandardCurrency(10000000)).toBe("₹​1,00,00,000");
+});
+
+test('Should return donation percent for 20% donation', () => {
+  expect(donationPerecent(10,2)).toBe(20);
+});
+test('Should return donation percent for some donations', () => {
+  expect(donationPerecent(10,5)).toBe(50);
 });
