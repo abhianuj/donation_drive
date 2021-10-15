01ee5334402408fb8ba7e91139994fa084631b79
I524896 <abhijeet.kumar04@sap.com>
2021-10-15 17:13:29 +0530

    copt to clipboard done

--- ui/src/Components/RecentFundRaiserCards.js
+++ ui/src/Components/RecentFundRaiserCards.js
@@ -1,7 +1,9 @@
-import { Card, CardMedia, CardContent, Typography, LinearProgress, Button } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, LinearProgress, Button, Dialog, DialogTitle,Snackbar, Alert, DialogContent, DialogContentText, DialogActions, Link } from "@mui/material";
 import ShareIcon from '@mui/icons-material/Share';
 import { returnFirstLetter, showIndianStandardCurrency, returnRemainingDaysLeft, donationPerecent } from './../utils/formatter';
 import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import { SERVER } from '../utils/api/services';
 
 const styles = {
     cardContaier: {
@@ -27,40 +29,100 @@ const styles = {
 
 const RecentFundRaiserCards = ({ fundraiserData }) => {
 
+    const [openShareDialog, setOpenShareDialog] = useState(false);
+    const [shareFundraiserData, setShareFundraiserData] = useState(null);
+    const [snackbar, setSnackbar] = useState({
+        severity: "success",
+        open: false,
+        snackbarText: ''
+    });
+
+    const handleDialogClose = () => {
+        setOpenShareDialog(false);
+    }
+    const share = (id) => {
+        setShareFundraiserData(SERVER + '/auth/donate/' + id);
+        setOpenShareDialog('true');
+    }
+    const handleCopyToClipboard = () => {
+        const cb = navigator.clipboard;
+        cb.writeText(shareFundraiserData).then(() => openSnackbar("URL copied to clipboard successfuly!", "success", true));
+    }
+
+    const handleSnackbarClose = () => {
+        setSnackbar({
+            severity: "success",
+            open: false,
+            snackbarText: ''
+        });
+    }
+    const openSnackbar = (text, severity, open) => {
+        setSnackbar({
+            severity: severity,
+            open: open,
+            snackbarText: text
+        });
+    }
+
     const history = useHistory();
     const donate = (id) => {
         history.push("/auth/donate/" + id);
     }
 
     return (
-        <Card sx={styles.cardContaier}>
-            <CardMedia
-            component="img"
-            height="140"
-            image={"data:image/png;base64," + fundraiserData.picture}
-            alt="green iguana"
-            />
-            <CardContent>
-                <Typography gutterBottom component="div" sx={styles.cardHeader}>
-                    {fundraiserData.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    <div style={{margin: '0.7rem 0 0.5rem 0'}}>
-                        <span style={styles.fundRaiserNameIcon}>{returnFirstLetter(fundraiserData.needy.firstName)}</span>
-                        <span style={{textTransform: 'capitalize'}}>{fundraiserData.needy.firstName.charAt(0).toUpperCase() + fundraiserData.needy.firstName.slice(1) + ' ' + fundraiserData.needy.lastName}</span>
-                    </div>
-                    <div style={{marginBottom: '0.2rem'}}>
-                        <span>{showIndianStandardCurrency(fundraiserData.totalDonation)}</span> Raised out of {showIndianStandardCurrency(fundraiserData.neededAmount)}
-                    </div>
-                    <LinearProgress sx={{height: '0.5rem', borderRadius: '50px', marginTop: '0.4rem'}} variant="determinate" value={donationPerecent(fundraiserData.neededAmount, fundraiserData.totalDonation)} />
-                    <div style={{margin: '0.8rem 0 0.5rem 0'}}><span>{returnRemainingDaysLeft(fundraiserData.endDate)}</span> days left</div>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <Button variant="outlined" disableElevation onClick={()=> {donate(fundraiserData.id)}}>Donate</Button>
-                        <Button variant="outlined" disableElevation startIcon={<ShareIcon style={{width: '1rem'}}/>}>Share</Button>
-                    </div>
-                </Typography>
-            </CardContent>
-        </Card>
+        <React.Fragment>
+            <Card sx={styles.cardContaier}>
+                <CardMedia
+                component="img"
+                height="140"
+                image={"data:image/png;base64," + fundraiserData.picture}
+                alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom component="div" sx={styles.cardHeader}>
+                        {fundraiserData.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        <div style={{margin: '0.7rem 0 0.5rem 0'}}>
+                            <span style={styles.fundRaiserNameIcon}>{returnFirstLetter(fundraiserData.needy.firstName)}</span>
+                            <span style={{textTransform: 'capitalize'}}>{fundraiserData.needy.firstName.charAt(0).toUpperCase() + fundraiserData.needy.firstName.slice(1) + ' ' + fundraiserData.needy.lastName}</span>
+                        </div>
+                        <div style={{marginBottom: '0.2rem'}}>
+                            <span>{showIndianStandardCurrency(fundraiserData.totalDonation)}</span> Raised out of {showIndianStandardCurrency(fundraiserData.neededAmount)}
+                        </div>
+                        <LinearProgress sx={{height: '0.5rem', borderRadius: '50px', marginTop: '0.4rem'}} variant="determinate" value={donationPerecent(fundraiserData.neededAmount, fundraiserData.totalDonation)} />
+                        <div style={{margin: '0.8rem 0 0.5rem 0'}}><span>{returnRemainingDaysLeft(fundraiserData.endDate)}</span> days left</div>
+                        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                            <Button variant="outlined" disabled={fundraiserData.neededAmount===fundraiserData.totalDonation} disableElevation onClick={()=> {donate(fundraiserData.id)}}>Donate</Button>
+                            <Button variant="outlined" onClick={()=> {share(fundraiserData.id)}} disableElevation startIcon={<ShareIcon style={{width: '1rem'}}/>}>Share</Button>
+                        </div>
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Dialog
+                open={openShareDialog}
+                onClose={handleDialogClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                Share the below link for donating.
+                </DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    <Link>{shareFundraiserData}</Link>
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleCopyToClipboard}>Copy to Clipboard</Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar open={snackbar.open} autoHideDuration={6000} onClose={handleSnackbarClose}>
+                <Alert onClose={handleSnackbarClose} severity={snackbar.severity} sx={{ width: '100%' }}>
+                    {snackbar.snackbarText}
+                </Alert>
+            </Snackbar>
+        </React.Fragment>
     );
 }
  
